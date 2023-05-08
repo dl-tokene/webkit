64b2a7b8140805af99c01c44e9697da0a2781452
lukachi <lukachisama@gmail.com>
2023-05-08 16:59:09 +0300

    redesigned toasts

--- packages/toasts/src/components/DefaultToast.vue
+++ packages/toasts/src/components/DefaultToast.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="notification">
-    <icon v-if="toastIcon" class="notification__icon" :name="toastIcon" />
-    <div class="notification__details">
-      <h4 class="notification__title">
+  <div class="default-toast__body">
+    <div class="default-toast__icon-wrp">
+      <icon v-if="toastIcon" class="default-toast__icon" :name="toastIcon" />
+    </div>
+    <div class="default-toast__details">
+      <h4 class="default-toast__title">
         {{ title }}
       </h4>
-      <p class="notification__message">
+      <p class="default-toast__message">
         {{ message }}
       </p>
     </div>
@@ -32,50 +34,104 @@ const toastIcon = computed(() => props.iconName || ICON_NAMES.checkFilled)
 </script>
 
 <style lang="scss">
-.Vue-Toastification__toast {
-  &--success {
-    background: var(--success-dark);
+.Vue-Toastification__toast.default-toast {
+  background: var(--white);
+  padding: toRem(12);
+}
+
+.default-toast__body {
+  display: flex;
+  gap: toRem(12);
+}
+
+.default-toast__icon-wrp {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: toRem(8);
+  min-width: toRem(56);
+  min-height: toRem(56);
+  max-width: toRem(56);
+  max-height: toRem(56);
+  width: toRem(56);
+  height: toRem(56);
+
+  .Vue-Toastification__toast--success & {
+    background: var(--success-light);
+    color: var(--success-main);
   }
 
-  &--error {
-    background: var(--error-dark);
+  .Vue-Toastification__toast--error & {
+    background: var(--error-light);
+    color: var(--error-main);
   }
 
-  &--warning {
-    background: var(--warning-dark);
+  .Vue-Toastification__toast--warning & {
+    background: var(--warning-light);
+    color: var(--warning-main);
   }
 
-  &--info {
+  .Vue-Toastification__toast--info & {
+    background: var(--primary-light);
+    color: var(--primary-main);
+  }
+}
+
+.default-toast .default-toast__icon {
+  max-width: toRem(24);
+  max-height: toRem(24);
+}
+
+.default-toast__details {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: toRem(8) 0;
+}
+
+.default-toast__title {
+  font-size: toRem(14);
+  line-height: 1.5;
+  font-weight: 500;
+  letter-spacing: toRem(0.1);
+  color: var(--text-primary-main);
+}
+
+.default-toast__message {
+  font-size: toRem(14);
+  line-height: 1.5;
+  letter-spacing: toRem(0.25);
+  color: var(--text-secondary-main);
+  width: toRem(230);
+
+  @include respond-to(xsmall) {
+    width: toRem(180);
+  }
+}
+
+.Vue-Toastification__close-button {
+  align-self: flex-start;
+  color: var(--text-secondary-main);
+  padding: 0;
+  font-size: toRem(28);
+  line-height: 1;
+}
+
+.Vue-Toastification__progress-bar {
+  .Vue-Toastification__toast--success & {
+    background: var(--success-main);
+  }
+
+  .Vue-Toastification__toast--error & {
+    background: var(--error-main);
+  }
+
+  .Vue-Toastification__toast--warning & {
+    background: var(--warning-main);
+  }
+
+  .Vue-Toastification__toast--info & {
     background: var(--primary-main);
   }
-}
-
-.notification {
-  display: grid;
-  place-items: flex-start;
-  grid-template-columns: max-content 1fr;
-  grid-gap: toRem(4);
-}
-
-.notification .notification__icon {
-  max-width: toRem(18);
-  max-height: toRem(18);
-}
-
-.notification__details {
-  display: grid;
-  grid-gap: toRem(4);
-  width: 100%;
-}
-
-.notification__title {
-  color: var(--text-primary-invert-main);
-  margin-bottom: toRem(12);
-}
-
-.notification__message {
-  color: var(--text-primary-invert-main);
-  line-height: 1.2;
-  max-width: toRem(200);
 }
 </style>
